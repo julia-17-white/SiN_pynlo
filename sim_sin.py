fixed bugs in dB squeezing calculation

sim_sin.py
@@ -57,11 +57,11 @@ def setup_waveguide_and_pulse():
     print("Expected P0 (W):", P0_expected)
 
     # SiN waveguide
-    thickness, width = 800e-9, 800e-9
+    thickness, width = 800e-9, 1000e-9
     # import ri_interpolator
     # sim_freqs = ri_interpolator.sim_freqs
     ## -- incorporating numpy mode files from abijith --
-    mode_file = 'from_abijith/jw_modes/JW_SiN_O2Clad_800nmThickness_800nmWidth_gamma_aeff.npy' 
+    mode_file = 'from_abijith/jw_modes/JW_SiN_O2Clad_800nmThickness_1000nmWidth_gamma_aeff.npy' 
     data = np.load(mode_file)
 
     # --- 1. Extract and Convert Data
@@ -121,10 +121,11 @@ def setup_waveguide_and_pulse():
     z_0 = np.pi * (t_fwhm/1.7627)**2 / (2 * np.abs(beta2_v0) )
 
     print(f"Beta2 at v0 ({v0*1e-12:.2f} THz): {beta2_v0:.3e} s^2/m")
-    print(f"Dispersion Length (L_D): {print("LD =", T0**2 / abs(np.average(beta2)))} m")
+    print(f"Dispersion Length (L_D): {print("LD =", T0**2 / abs(np.mean(beta2)))} m")
     print(f"Soliton Period (z_0): {z_0:.5f} m")
     length = 0.003
     print(f'Number of soliton periods: {length/z_0}')
+    print(f'typical gamma: {np.mean(gamma_data)}')
 
     return pulse, mode, v_grid
 
@@ -361,16 +362,17 @@ def init_worker():
     This runs once on each CPU core when the process pool spawns.
     It initializes the un-picklable pynlo objects locally on that core.
     """
-    global _worker_pulse, _worker_mode, _worker_v_grid, _worker_a_v_lo
+    global _worker_pulse, _worker_mode, _worker_v_grid, _worker_a_v_lo, _worker_a_v_clean_out
     
     # Generate the base pulse and mode directly on this core
     _worker_pulse, _worker_mode, _worker_v_grid = setup_waveguide_and_pulse()
+    _worker_a_v_lo = copy.deepcopy(_worker_pulse.a_v)
+    clean_pulse = copy.deepcopy(_worker_pulse)
     
     # Generate the clean Local Oscillator profile directly on this core
     print("Calculating clean LO pulse on each core...")
-    _, _, _, a_v_clean_out, _ = propagate_pulse(_worker_pulse, _worker_mode)
-    _worker_a_v_lo = a_v_clean_out[-1]
-
+    _, _, _, a_v_clean, _ = propagate_pulse(clean_pulse, _worker_mode)
+    _worker_a_v_clean_out = a_v_clean[-1]
 
 
 
@@ -414,7 +416,7 @@ def run_single_iteration(iteration_index):
     if iteration_index == 2:
         s = time.time()
 
-    global _worker_pulse, _worker_mode, _worker_v_grid, _worker_a_v_lo
+    global _worker_pulse, _worker_mode, _worker_v_grid, _worker_a_v_lo, _worker_a_v_clean_out
 
     local_rng = np.random.default_rng(seed_value + iteration_index)
 
@@ -432,7 +434,8 @@ def run_single_iteration(iteration_index):
     
     # Propagate the noisy pulse
     new_pulse, z, a_t, a_v_out, sim = propagate_pulse(noisy_pulse, _worker_mode)
-    c_sig = np.sum(a_v_out[-1] * np.conj(_worker_a_v_lo))
+    delta_a_v = a_v_out[-1] - _worker_a_v_clean_out # not sure if this step is necessary but it is technically isolating the noise fluctuations
+    c_sig = np.sum(delta_a_v * np.conj(_worker_a_v_lo))
     
     if iteration_index == 2:
         print(f'One iteration run time: {time.time() - s} s')