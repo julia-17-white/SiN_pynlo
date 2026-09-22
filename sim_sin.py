aec41fe0a1f71d3aa4624856db5377b3888ecf08
Diddams <julia.17.white@gmail.com>
2026-09-22 09:23:26 -0600

    finished the initial input with scan

--- sim_sin.py
+++ sim_sin.py
@@ -734,17 +734,17 @@ def sim_with_noise_parallel(gd, file, length, pwr_ratio, fin_data, fig_path, pow
 
 
 def main():
-    # widths = ['800', '1000', '1200', '1400', '1600', '1800', '2000', '2200', '2400', '2600', '2800', '3000', '3200', '3400', '3600',
-    #           '3800', '4000', '4200', '4400', '4600', '4800', '5000'] # '800', '1000', 
-    widths = ['1400']
+    widths = ['800', '1000', '1200', '1400', '1600', '1800', '2000', '2200', '2400', '2600', '2800', '3000', '3200', '3400', '3600',
+              '3800', '4000', '4200', '4400', '4600', '4800', '5000'] # '800', '1000', 
+    # widths = ['1400']
     # power_list = [5, 10, 15, 20, 25, 30, 35, 40, 50, 100, 150, 200, 250, 300, 350, 400]
     power_list = [40]
-    # fwhm_list = [.208, .4, .6, .8, 1.0, 1.5, 2.0]
-    fwhm_list = [.4]
+    fwhm_list = [.208, .4, .6, .8, 1.0, 1.5, 2.0]
+    # fwhm_list = [.4]
     file_path1 = 'from_abijith/jw_modes/JW_SiN_O2Clad_800nmThickness_' 
     file_path2 = 'nmWidth_gamma_aeff.npy'
 
-    length = .02 #m
+    length = .10 #m
     pwr_ratio = .01
 
     # It is highly recommended to disable OpenMP threading when using ProcessPoolExecutor
